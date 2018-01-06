88a3add0b616f7853c7ce67ee3d4e71b0c0dfa21
balbes150 <balbes-150@yandex.ru>
2018-01-07 00:49:47 +0300

    fix

--- scripts/aml812armv7config.sh
+++ scripts/aml812armv7config.sh
@@ -8,7 +8,7 @@ echo "Creating \"fstab\""
 echo "# Amlogic fstab" > /etc/fstab
 echo "" >> /etc/fstab
 echo "proc            /proc           proc    defaults        0       0
-UUID=${UUID_BOOT} /boot           vfat    defaults,utf8,user,rw,umask=111,dmask=000        0       1
+#UUID=${UUID_BOOT} /boot           vfat    defaults,utf8,user,rw,umask=111,dmask=000        0       1
 tmpfs   /var/log                tmpfs   size=20M,nodev,uid=1000,mode=0777,gid=4, 0 0
 tmpfs   /var/spool/cups         tmpfs   defaults,noatime,mode=0755 0 0
 tmpfs   /var/spool/cups/tmp     tmpfs   defaults,noatime,mode=0755 0 0
@@ -18,11 +18,12 @@ tmpfs   /dev/shm                tmpfs   defaults,nosuid,noexec,nodev        0 0
 
 echo "#!/bin/sh -e
 /etc/hdmi.sh &
+dhclient eth0 &
 exit 0" > /etc/rc.local
 
 echo "Installing additonal packages"
 apt-get update
-apt-get -y install u-boot-tools liblircclient0 lirc mc
+apt-get -y install u-boot-tools liblircclient0 lirc mc abootimg fbset
 
 echo "Cleaning APT Cache and remove policy file"
 rm -f /var/lib/apt/lists/*archive*
